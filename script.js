fd97d7c428f5c31de9a8dfecccc96c8ac33e2de4
Akshay0932 <97750010+Akshay0932@users.noreply.github.com>
2022-02-24 12:49:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function upvote()
+{
+    let votes = document.getElementById('votes');// will give line 72
+    let count = parseInt(votes.innerHTML); // "1000" -> 1000
+    count++; // 1000 -> 1001
+    votes.innerHTML = count;   
+}
+
+function downvote()
+{
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML); // "1000" -> 1000
+    count--; // 1000 -> 999
+    votes.innerHTML = count;   
+}
 
